b6e9cbb1d9e957a336f2282103c6a8f18a5f0721
Myles Williams <mwilliams.sfo@gmail.com>
2023-06-20 12:26:58 -0700

    Write a string

--- src/boot.s
+++ src/boot.s
@@ -1,9 +1,24 @@
 .org 0
 .code16
-mov $0xe, %ah
-mov $'.', %al
-int $0x10
-jmp .
+	movw $0x7c0, %ax
+	movw %ax, %ds
+	movw $str, %si
+	call print
+	jmp .
+
+print:	lodsb
+	testb %al, %al
+	jz 1f
+	call putc
+	jmp print
+1:	ret
+
+putc:	movb $0xe, %ah
+	int $0x10
+	ret
+
+str:
+.asciz "Done"
 
 .org 510
-.byte 0x55, 0xaa
+.word 0xaa55
