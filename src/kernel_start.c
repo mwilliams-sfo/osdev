1485fa333a22a6ed2f8e74102dd9efc4a1eb93e9
Myles Williams <mwilliams.sfo@gmail.com>
2023-06-28 15:43:55 -0700

    Add basic output functions

--- src/kernel_start.c
+++ src/kernel_start.c
@@ -1,18 +1,63 @@
 
+#include <stdint.h>
+
 #define TEXT_SCREEN_BASE 0xb8000
 #define TEXT_SCREEN_WIDTH 80
 #define TEXT_SCREEN_HEIGHT 25
 
-static const char * const msg = "Kernel started";
+static uint16_t * const vid_mem = (uint16_t *) TEXT_SCREEN_BASE;
+static int terminal_row = 0;
+static int terminal_col = 0;
+
+static void tinit(void)
+{
+	for (int i = 0; i < TEXT_SCREEN_WIDTH * TEXT_SCREEN_HEIGHT; i++) {
+		vid_mem[i] = (7 << 8) | ' ';
+	}
+}
+
+static void tplot(int x, int y, char c, int color)
+{
+	if (x < 0 || TEXT_SCREEN_WIDTH <= x || y < 0 || TEXT_SCREEN_HEIGHT <= y)
+		return;
+	vid_mem[y * TEXT_SCREEN_WIDTH + x] = (color << 8) | c;
+}
+
+static void tputc(char c, int color)
+{
+	if (terminal_row == TEXT_SCREEN_HEIGHT) {
+		for (int i = 0; i < (TEXT_SCREEN_HEIGHT - 1) * TEXT_SCREEN_WIDTH; i++) {
+			vid_mem[i] = vid_mem[i + TEXT_SCREEN_WIDTH];
+		}
+		terminal_row--;
+	}
+	switch (c) {
+	case '\r':
+		terminal_col = 0;
+		return;
+	case '\n':
+		terminal_col = 0;
+		terminal_row++;
+		return;
+	}
+	tplot(terminal_col, terminal_row, c, color);
+	terminal_col++;
+	if (terminal_col == TEXT_SCREEN_WIDTH) {
+		terminal_col = 0;
+		terminal_row++;
+	}
+}
+
+static void tputs(const char * s, int color)
+{
+	for (int i = 0; s[i]; i++) {
+		tputc(s[i], color);
+	}
+}
 
 void kernel_start()
 {
-	volatile char * const vid_mem = (char *) TEXT_SCREEN_BASE;
-	for (int i = 0; i < TEXT_SCREEN_WIDTH * TEXT_SCREEN_HEIGHT;) {
-		vid_mem[i++] = ' ';
-		vid_mem[i++] = 7;
-	}
-	for (int i = 0; msg[i]; i++) {
-		vid_mem[i * 2] = msg[i];
-	}
+	tinit();
+	tputs("Kernel started\n", 15);
+	tputs("Done\n", 15);
 }
