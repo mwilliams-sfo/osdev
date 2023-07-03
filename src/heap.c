da07c6ea47a6f32af52d6eb1f3b7d50be3a4db72
Myles Williams <mwilliams.sfo@gmail.com>
2023-07-03 12:08:02 -0700

    Replace bit operations with bit fields in heap

--- src/heap.c
+++ src/heap.c
@@ -4,59 +4,62 @@
 
 #define HEAP_SIZE (100U << 20)
 #define HEAP_BLOCK_SIZE 4096U
-#define HEAP_BLOCK_FLAG_FIRST 0x80
-#define HEAP_BLOCK_FLAG_HASN 0x40
-#define HEAP_BLOCK_TYPE_MASK 0xf
 #define HEAP_BLOCK_TYPE_FREE 0
 #define HEAP_BLOCK_TYPE_USED 1
 #define HEAP_TABLE_SIZE (HEAP_SIZE / HEAP_BLOCK_SIZE)
 #define HEAP_BASE_ADDRESS 0x01000000U
 
-static uint8_t heap_table[HEAP_SIZE / HEAP_BLOCK_SIZE];
+struct heap_entry {
+	uint8_t type: 4;
+	uint8_t: 2;
+	uint8_t has_next: 1;
+	uint8_t first: 1;
+} __attribute__((packed));
+
+static struct heap_entry heap_table[HEAP_SIZE / HEAP_BLOCK_SIZE];
 uint8_t (*heap_region)[HEAP_BLOCK_SIZE] = (uint8_t (*)[HEAP_BLOCK_SIZE]) HEAP_BASE_ADDRESS;
 
 void * heap_alloc(unsigned size) {
-	int n, i, j;
-	void * ptr;
-
 	if (size == 0) return NULL;
 	size += (~size + 1) & (HEAP_BLOCK_SIZE - 1);
-	n = size >> 12;
+	int n = size >> 12;
 
 	// Find the necessary free blocks.
-	i = 0;
+	int i = 0, j;
 	for (j = 0; j < i + n; j++) {
 		if (j == sizeof heap_table / sizeof heap_table[0]) return NULL;
-		if ((heap_table[j] & HEAP_BLOCK_TYPE_MASK) == HEAP_BLOCK_TYPE_USED) {
+		if (heap_table[j].type == HEAP_BLOCK_TYPE_USED) {
 			i = j + 1;
 		}
 	}
 
 	// Allocate the blocks.
-	ptr = &heap_region[i];
-	heap_table[i] = HEAP_BLOCK_FLAG_FIRST;
-	for(; i < j; i++) {
-		heap_table[i] |= HEAP_BLOCK_FLAG_HASN | HEAP_BLOCK_TYPE_USED;
+	void * ptr = &heap_region[i];
+	struct heap_entry * entry = &heap_table[i];
+	entry->first = 1;
+	for(; i < j; i++, entry++) {
+		entry->type = HEAP_BLOCK_TYPE_USED;
+		entry->has_next = 1;
 	}
-	heap_table[i - 1] &= ~HEAP_BLOCK_FLAG_HASN;
+	(entry - 1)->has_next = 0;
 	return (void *) ptr;
 }
 
 void heap_free(void * ptr) {
-	int i;
-
 	// Sanity-check the address.
 	if (ptr < (void *) heap_region) return;
 	if ((unsigned) ptr != ((unsigned) ptr & (HEAP_BLOCK_SIZE - 1))) return;
-	i = (unsigned) ptr >> 12;
+	int i = (unsigned) ptr >> 12;
 	if (i >= sizeof heap_table / sizeof heap_table[0]) return;
-	if ((heap_table[i] & HEAP_BLOCK_FLAG_FIRST) == 0) return;
+	struct heap_entry * entry = &heap_table[i];
+	if (!entry->first) return;
 
 	// Unallocate blocks.
 	while (1) {
 		if (i == sizeof heap_table / sizeof heap_table[0]) return;
-		if ((heap_table[i] & HEAP_BLOCK_FLAG_HASN) == 0) break;
-		heap_table[i++] = 0;
+		if (!entry->has_next) break;
+		*entry = (struct heap_entry) { 0 };
+		i++; entry++;
 	}
-	heap_table[i] = 0;
+	*entry = (struct heap_entry) { 0 };
 }
